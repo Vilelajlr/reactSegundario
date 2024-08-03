e9daec8917716efefa5b6ef63d96b65a389460be
José Leandro Vilela <joseleandro2807@gmail.com>
2024-08-02 23:08:14 -0300

    project costs

--- arquivos/costs/src/App.js
+++ arquivos/costs/src/App.js
@@ -1,26 +1,34 @@
-import{BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './components/pages/Home'
-import Company from './components/pages/Company'
-import Contact from  './components/pages/Contact'
-import NewProject from './components/pages/NewProject'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import NewProject from "./components/pages/NewProject";
+import Contact from "./components/pages/Contact";
+import Projects from "./components/pages/Projects";
 
+import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <Router>
-      
+
+      <Navbar />
 
       <Container customClass="min-height">
-        <Routes> 
-          <Route exact path="/" element={<Home />} />
-          <Route path="/company" element={<Company />} />
-          <Route path="/contato" element={<Contact />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={ <Projects /> } />
+          <Route path="/company" element={ <Company /> } />
           <Route path="/newproject" element={<NewProject />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Container>
 
       <Footer />
+      
+
     </Router>
   );
 }
